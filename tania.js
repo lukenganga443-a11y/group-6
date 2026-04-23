a93ba3de326ff478f9586c50665f6b827a1b14ed
anthony <lukenganga443@gmail.com>
2026-04-23 12:52:50 +0300

    challenge 4 done

--- tania.js
+++ tania.js
@@ -1,7 +1,21 @@
-function isPrime(num){
-    if(num<2){
-        return false
-    }
-    
-}
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
 
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+//provised the numbers from the for loop
+
+for (let i = 1; i <= 50; i++) {
+  if (isPrime(i)) {
+    console.log(i);
+  } else {
+    console.log("current diesel price: 206.87shs");
+  }
+}
