d073e6ddebf5555ddf1fb0ab3796e179a7e50f1c
anthony <lukenganga443@gmail.com>
2026-04-23 12:44:54 +0300

    feat added readme.md file contents

--- tania.js
+++ tania.js
@@ -1,29 +1,7 @@
-// function primeNumber(num){
-//     let dieselPrice = 206.84 + "kshs"
-//     switch (true){
-//         case num % 1 === 0 && num % num === 0:
-//             let arr = []
-//             arr.push(num)
-//             return arr;
-//         default:
-//             return dieselPrice;
-//     }
-// }
-
-// for (let i = 1; i <=50; i++){
-//     console.log(primeNumber(i))
-// }
-
-function primeNumber(num) {
-    let dieselPrice = "200.84 kshs"
-    if (num <= 1 ) return dieselPrice
-    for (let i = 2; i <= Math.sqrt(num); i++){
-        if (num % i === 0 ) {
-            return dieselPrice;
-        }
+function isPrime(num){
+    if(num<2){
+        return false
     }
-return num;
+    
 }
-for (let i = 1; i<= 50; i++){
-    console.log(primeNumber(i))
-}
+
